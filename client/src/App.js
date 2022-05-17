edf3dde3a4ab4ea164e91bc4d921a589cb15ca0a
Kevin Li <kevinli5370@live.com>
2022-05-17 15:44:26 -0400

    Move routing from index.js to App.js to easily pass props to children

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 import React from "react";
-import { Outlet, Link, NavLink } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  NavLink
+} from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import SearchPage from "./routes/search-page";
+import BrowsePage from "./routes/browse-page";
 
 class App extends React.Component {
   constructor(props) {
@@ -30,21 +39,29 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand as={Link} to="/">AmbienceGen</Navbar.Brand>
-          <Nav>
-            {this.renderMyAmb()}
-            <Nav.Item>
-                <Nav.Link as={NavLink} to="/browse">Browse</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={NavLink} to="/search">Search</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar>
-        <Outlet />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand as={Link} to="/">AmbienceGen</Navbar.Brand>
+            <Nav>
+              {this.renderMyAmb()}
+              <Nav.Item>
+                  <Nav.Link as={NavLink} to="/browse">Browse</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                  <Nav.Link as={NavLink} to="/search">Search</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar>
+          <Outlet />
+        </div>
+        <Routes>
+          <Route path="search" element={<SearchPage loggedIn={this.state.loggedIn} />} />
+          <Route path="browse" element={<BrowsePage loggedIn={this.state.loggedIn} />} />
+          <Route path="*" element={<main style={{ padding:"1rem"}}><p>There's nothing here!</p></main>} />
+        </Routes>
+
+      </BrowserRouter>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
 import App from './App';
-import SearchPage from "./routes/search-page";
-import BrowsePage from "./routes/browse-page";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="search" element={<SearchPage />} />
-        <Route path="browse" element={<BrowsePage />} />
-        <Route path="*" element={<main style={{ padding:"1rem"}}><p>There's nothing here!</p></main>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
