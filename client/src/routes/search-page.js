599705cbbbcbd96b7a050dc66b71f8c5fc7823f7
Kevin Li <kevinli5370@live.com>
2022-06-21 16:25:40 -0400

    Create client/src/routes/search-page.js

--- client/src/routes/search-page.js
+++ client/src/routes/search-page.js
@@ -1,8 +1,85 @@
-function SearchPage() {
+import { useState } from 'react';
+import AmbTable from '../components/amb-table';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+
+function SearchPage(props) {
+  const [ambList, setAmbList] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getSearchResults = () => {
+    console.log("Searching for \"" + searchTerm +"\"");
+    fetch("http://localhost:3001/browse/?search="+searchTerm)
+      .then(res => {
+        if(res.status >= 400)
+          throw new Error('Server error!');
+        return res.json();
+      })
+      .then(result => {
+        setAmbList(result);
+      })
+      .catch(error => {
+        console.error(error);
+        setAlertMessage(error.message);
+        setShowAlert(true);
+      })
+  };
+
+  const renderAlert = () => {
+    if(showAlert) {
+      return (
+        <Alert
+          className="my-2"
+          variant="danger"
+        >
+          <Row>
+            <Col className="text-center">
+              {alertMessage}
+            </Col>
+          </Row>
+        </Alert>
+      );
+    } else {
+      return null;
+    }
+  }
+
   return (
-    <main style={{ padding: "1rem 0"}}>
-      <h2>Search</h2>
-    </main>
+    <Container>
+      <Row className="text-center mt-2">
+        <h2>Search</h2>
+      </Row>
+      {renderAlert()}
+      <Row>
+        <Col>
+          <Form className="my-2">
+            <Form.Group controlId="formSearchTerm">
+              <Form.Control
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col xs="auto">
+          <Button
+            className="my-2"
+            onClick={() => getSearchResults()}
+          >
+            Search
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <AmbTable ambList={ambList} />
+      </Row>
+    </Container>
   );
 }
 
