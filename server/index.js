2d1b5e3c106857e08c15b2864750afe68b7891a4
Kevin Li <kevinli5370@live.com>
2022-07-06 13:07:49 -0400

    Remove strict mode on client/src/index.js Remove .env in server/index.js Fix db creation bugs

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-console.log(require('dotenv').config());
+// console.log(require('dotenv').config());
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
