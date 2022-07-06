29d3a050c62d497f29e59c1d2010eb92d302c3e8
Kevin Li <kevinli5370@live.com>
2022-07-06 14:17:56 -0400

    Add route to send static React index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const cors = require("cors");
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const user = require('./routes/users-route.js');
 const amb = require('./routes/ambs-route.js');
@@ -14,6 +15,13 @@ app.use(bodyParser.json());
 app.use('/user', user);
 app.use('/amb', amb);
 app.use('/browse', browse);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
 
 app.use((err, req, res, next) => {
   console.error(err);
