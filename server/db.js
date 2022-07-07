def7534d78df6f78a4f713a53a82364141188aa1
Kevin Li <kevinli5370@live.com>
2022-07-07 16:17:06 -0400

    Change db cn options depending on NODE_ENV

--- server/db.js
+++ server/db.js
@@ -3,9 +3,16 @@ const initOptions = {
 };
 
 const pgp = require('pg-promise')(initOptions);
-const cn = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+let cn;
+if(process.env.NODE_ENV === 'production') {
+  cn = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  }
+} else {
+  cn = {
+    connectionString: process.env.DATABASE_URL,
+  }
 }
 const db = pgp(cn);
 
